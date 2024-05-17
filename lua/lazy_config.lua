c401953f07bbe23caf6777a458b51069ba64ff21
Steven <steven.j.moe@gmail.com>
2024-05-17 10:48:56 +1000

    Add some more cmp plugins

--- lua/lazy_config.lua
+++ lua/lazy_config.lua
@@ -35,6 +35,8 @@ require("lazy").setup({
 	{ 'hrsh7th/cmp-nvim-lsp' },
 	{ 'hrsh7th/nvim-cmp' },
 	{ 'hrsh7th/cmp-buffer' },
+	{ 'hrsh7th/cmp-path' },
+	{ 'hrsh7th/cmp-cmdline' },
 	{ 'L3MON4D3/LuaSnip' },
 
 	-- git
