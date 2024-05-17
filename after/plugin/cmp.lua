4c938119b7223278c7c482f5a97f2fa2851e482b
Steven <dev@stevenm.co>
2024-05-17 12:39:49 +1000

    Remove lspconfig for now. Was breaking stuff

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -45,8 +45,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['svelte-language-server'].setup {
-  capabilities = capabilities
-}
+--local capabilities = require('cmp_nvim_lsp').default_capabilities()
+--require('lspconfig')['svelte'].setup {
+--	capabilities = capabilities
+--}
