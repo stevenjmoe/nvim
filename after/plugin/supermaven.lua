d7690aab31d7c508b44a2d8453bf9d72410877a7
Steven <dev@stevenm.co>
2024-08-08 06:44:09 +1000

    remove broken supermaven config

--- after/plugin/supermaven.lua
+++ after/plugin/supermaven.lua
@@ -2,16 +2,3 @@ require("supermaven-nvim").setup({
 	disable_keymaps = true,
 	color = "#ffffff",
 })
-
-M.expand = function(fallback)
-	local luasnip = require('luasnip')
-	local suggestion = require('supermaven-nvim.completion_preview')
-
-	if luasnip.expandable() then
-		luasnip.expand()
-	elseif suggestion.has_suggestion() then
-		suggestion.on_accept_suggestion()
-	else
-		fallback()
-	end
-end
