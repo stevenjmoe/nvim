89acc6d5a479a7568502a5791dde978022ae2f36
Steven Moe <dev@stevenm.co>
2025-01-18 15:15:02 +1100

    add love2d plugin

--- lua/lazy_config.lua
+++ lua/lazy_config.lua
@@ -223,4 +223,14 @@ require("lazy").setup({
 			"rcarriga/nvim-notify",
 		}
 	},
+	{
+		"S1M0N38/love2d.nvim",
+		cmd = "LoveRun",
+		opts = {},
+		keys = {
+			{ "<leader>v",  ft = "lua",          desc = "LÖVE" },
+			{ "<leader>vv", "<cmd>LoveRun<cr>",  ft = "lua",   desc = "Run LÖVE" },
+			{ "<leader>vs", "<cmd>LoveStop<cr>", ft = "lua",   desc = "Stop LÖVE" },
+		},
+	}
 })
