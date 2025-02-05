fe2054a3baed647d877c3799fa4cbe09b3ea326f
Steven <smoe@sunitafe.edu.au>
2025-02-06 08:50:30 +1100

    set fsautocomplete command correctly

--- after/plugin/ionide.lua
+++ after/plugin/ionide.lua
@@ -1,14 +1,16 @@
 -- Basic Ionide settings
-vim.g.fsharp_automatic_sln_loading = 1
-vim.g.fsharp_map_namespaces = 1
-vim.g.fsharp_map_typecheck = 1
-vim.g.fsharp_map_gotodecl = 1
-vim.g.fsharp_map_gotonext = 1
+--vim.g.fsharp_automatic_sln_loading = 1
+--vim.g.fsharp_map_namespaces = 1
+--vim.g.fsharp_map_typecheck = 1
+--vim.g.fsharp_map_gotodecl = 1
+--vim.g.fsharp_map_gotonext = 1
 
-vim.g.fsharp_diagnostic_mode = "all"
-vim.g.fsharp_resolve_namespaces = 1
-vim.g.fsharp_enable_reference_code_lens = 1
-vim.g.fsharp_enable_analyzers = 1
+--vim.g.fsharp_diagnostic_mode = "all"
+--vim.g.fsharp_resolve_namespaces = 1
+--vim.g.fsharp_enable_reference_code_lens = 1
+--vim.g.fsharp_enable_analyzers = 1
+
+vim.g["fsharp#fsautocomplete_command"] = { "dotnet", "fsautocomplete" }
 
 -- File type specific settings
 vim.api.nvim_create_autocmd("FileType", {
